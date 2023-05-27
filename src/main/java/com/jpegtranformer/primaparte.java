3d1d60b71fcd455e2d9b7c0ad90396eff0474fe8
Gabriele Lecchi <gabriele.lecchi@gmail.com>
2023-05-27 12:28:08 +0200

    aggiunta libreria jtransform

--- src/main/java/com/jpegtranformer/primaparte.java
+++ src/main/java/com/jpegtranformer/primaparte.java
@@ -1,12 +1,28 @@
 package com.jpegtranformer;
 
+import org.jtransforms.dct.DoubleDCT_2D;
 
 public class primaparte {
     public static void main(String[] args) {
-
+        int n = 8;
         // double[][] f = new double[n][n];
-        double[][] f = { { 8, 2, 3 }, { 5, 2, 6 }, { 7, 8, 9 } };
+        double[][] f = { { 231, 32, 233, 161, 24, 71, 140, 245 }, { 247, 40, 248, 245, 124, 204, 36, 107 },
+                { 234, 202, 245, 167, 9, 217, 239, 173 }, { 193, 190, 100, 167, 43, 180, 8, 70 },
+                { 11, 24, 210, 177, 81, 243, 8, 112 }, { 97, 195, 203, 47, 125, 114, 165, 182 },
+                { 193, 70, 174, 167, 41, 30, 127, 245 }, { 87, 149, 57, 192, 65, 129, 178, 228 } };
+        System.out.println("nostra:");
         mineDtc2(f);
+        DoubleDCT_2D dct = new DoubleDCT_2D(n, n);
+        dct.forward(f, true);
+        System.out.println();
+        System.out.println();
+        System.out.println("libreria:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.printf("%f\t", f[i][j]);
+            System.out.println();
+        }
+
     }
 
     public static void mineDtc2(double[][] f) {
@@ -32,7 +48,6 @@ public class primaparte {
                 } else {
                     alpha[k][l] = 2 / n;
                 }
-                System.out.println(alpha[k][l]);
                 c[k][l] = alpha[k][l] * res;
             }
         }
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.jpegtranformer {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.jpegtranformer to javafx.fxml;
-    exports com.jpegtranformer;
-}
